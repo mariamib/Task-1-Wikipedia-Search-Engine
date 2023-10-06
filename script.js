428193ae72cc44ed5c5a392496507237787a192f
Mariam Ibrahem <70824552+mariamib@users.noreply.github.com>
2023-10-06 22:54:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let resultsContainer = document.getElementsByClassName("container")[0]
+let timer;
+const generateResults = (searchValue, inputField) => {
+    console.log(1)
+    fetch(
+        "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
+        + searchValue
+    )
+    .then(response => response.json())
+    .then(data => {
+        let results = data.query.search
+        let numberOfResults = data.query.search.length
+        resultsContainer.innerHTML = ""
+        for(let i=0; i<numberOfResults; i++) {
+            let result = document.createElement("div")
+            result.classList.add("results")
+            result.innerHTML = `
+            <div>
+                <h3>${results[i].title}</h3>
+                <p>${results[i].snippet}</p>
+            </div>
+            <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
+            `
+            resultsContainer.appendChild(result)
+        }
+        if(inputField.value === ""){
+            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
+        }
+    })
+}
+const validateInput = (el) => {
+    if(el.value === ""){
+        resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
+    }else{
+      
+        clearTimeout(timer); 
+        timer = setTimeout(() => {
+            generateResults(el.value, el); 
+        }, 2000);
+    }
+}
+
